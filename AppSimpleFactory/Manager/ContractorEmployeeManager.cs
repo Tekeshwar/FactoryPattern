662a05e5912f59fc971cd8099307233079903bf8
Tekeshwar Bisen <tekeshpixel@gmail.com>
2020-10-30 23:21:49 +0530

    Simple factory

---
+++ AppSimpleFactory/Manager/ContractorEmployeeManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppSimpleFactory.Manager
+{
+    public class ContractorEmployeeManager : IEmployeeManager
+    {
+        decimal IEmployeeManager.GetBonus()
+        {
+            return 20;
+        }
+
+        decimal IEmployeeManager.GetPay()
+        {
+            return 10;
+        }
+    }
+}
